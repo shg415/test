8a31f0896e536ac952f0eb1fbd60f9845b2260c9
shg415 <shg415@gmail.com>
2019-08-21 12:44:15 +0900

    update

--- nuxt-project-sample/components/Logo.vue
+++ nuxt-project-sample/components/Logo.vue
@@ -1,8 +1,9 @@
 <template>
-  <div id="app">
+  <div id="news">
     <ul>
       <li v-for="item in items">
-        {{ item.message }}
+        <h2>{{ item.title }}</h2>
+        <p>{{ item.from }}{{ item.date }}<span>{{ item.point }}points</span></p>
       </li>
     </ul>
 
@@ -17,10 +18,66 @@
     data() {
       return{
         items: [
-        { message: 'りんご' },
-        { message: 'いちご' }
+        { title: "りんご", from:"TechCrunch Japan", date: "2019.08.20", point: 3},
+        { title: "りんご", from:"TechCrunch Japan", date: "2019.08.20", point: 3},
+        { title: "りんご", from:"TechCrunch Japan", date: "2019.08.20", point: 3},
+        { title: "りんご", from:"TechCrunch Japan", date: "2019.08.20", point: 3}
         ]
       };
     }
   };
 </script>
+
+
+
+<style scoped lang="scss">
+
+    ul{
+      margin: 0 auto;
+      padding: 0;
+    li{
+      margin-bottom: 3px;
+      background-color: white;
+      list-style: none;
+      padding: 10px 30px;
+
+
+      h2{
+        font-weight:normal;
+        font-size: 14px;
+        text-align: left;
+        position:relative;
+        &:before{
+        content: "";
+        position:absolute;
+        left:-20px;
+        top:2px;
+        width:15px;
+        height:15px;
+        display: block;
+        background: url('~assets/img/list-icon.png');
+        background-size:contain;
+        }
+      }
+      p{
+        font-size: 8px;
+        color: gray;
+        text-align: left;
+        line-height: 16px;
+
+        span{
+          $background: #F5F5F5;
+          color: #4394BA;
+          font-size: 11px;
+          background-color: $background;
+          margin: 0 5px;
+          padding: 1px 3px;
+        }
+      }
+    }
+
+
+    }
+
+
+</style>
--- nuxt-project-sample/pages/index.vue
+++ nuxt-project-sample/pages/index.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="container">
     <div>
+      <div id="app">
+      <button-vue>aaa</button-vue>
+      </div>
+      <heading-h1 />
       <logo />
       <h1 class="title">
         nuxt-project-sample
@@ -10,63 +14,83 @@
       </h2>
       <div class="links">
         <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
+        href="https://nuxtjs.org/"
+        target="_blank"
+        class="button--green"
         >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+        Documentation
+      </a>
+      <a
+      href="https://github.com/nuxt/nuxt.js"
+      target="_blank"
+      class="button--grey"
+      >
+      GitHub
+    </a>
   </div>
+</div>
+</div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+  import Logo from '~/components/Logo.vue'
+  import HeadingH1 from '~/components/HeadingH1.vue'
 
-export default {
-  components: {
-    Logo
+  export default {
+    components: {
+      Logo,
+      HeadingH1
+    }
   }
-}
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+<script>
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+  Vue.component("button-vue", {
+    template: `
+    <div class="button-vue">
+      <slot></slot>
+    </div>
+    `
+  });
+  var app = new Vue({
+    el: "#app"
+  });
+</script>
+
+
+
+<style scoped lang="scss">
+  .container {
+    $background: #F5F5F5;
+    margin: 0 auto;
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    background-color:$background;
+  }
+
+  .title {
+    font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
     'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
+    display: block;
+    font-weight: 300;
+    font-size: 100px;
+    color: #35495e;
+    letter-spacing: 1px;
+  }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
+  .subtitle {
+    font-weight: 300;
+    font-size: 42px;
+    color: #526488;
+    word-spacing: 5px;
+    padding-bottom: 15px;
+  }
 
-.links {
-  padding-top: 15px;
-}
+  .links {
+    padding-top: 15px;
+  }
 </style>
